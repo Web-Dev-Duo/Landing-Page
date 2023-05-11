461ea1d1a8b1844c785fb454f94794d2ce7f439a
Shashank S Nayak <shashank96.nayak@gmail.com>
2023-05-11 07:09:33 +0530

    LP-4 | Add code for main page (#2)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.scss';
 import Header from './components/header/header';
+import codeThinking from './assets/codeThinking.svg';
+import namaste from './assets/namaste.svg';
 
 function App() {
   return (
-	<div class="main">
+	<div className="main">
 		<Header></Header>
+		<div className="heroContainer">
+			<div className="leftHeroDiv">
+				<span className="boldText">
+					Namaste
+				</span>
+				<img className="namasteImage" src={namaste}></img>
+				<p>
+					Where ideas become reality
+				</p>
+				<div className="contactButton">
+					<a>CONTACT US</a>
+				</div>
+			</div>
+			<div className="rightHeroDiv">
+				<img src={codeThinking} />
+			</div>
+		</div>
+		<div className="mainContent2">
+		</div>
 	</div>
   );
 }
